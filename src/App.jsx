abce5fd1553879a3732c78d0adf03aa045598639
paulyatim <jeanpaulyatimg@gmail.com>
2024-02-01 20:13:08 -0300

    version mobile agregada

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import Content from "./components/Content";
-import { BrowserRouter } from 'react-router-dom'
+import Responsive from "./components/Responsive";
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import React from "react";
 document.body.style.backgroundColor = "black";
 
 function App() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 768;
+  React.useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+  if (width > breakpoint) {
+    return (
+      <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
+        <div>
+          <Content className="App"></Content>
+        </div>
+      </BrowserRouter>
+    );
+  } 
   return (
     <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
       <div>
-        <Content className="App"></Content>
+        <Responsive className="App"></Responsive>
       </div>
     </BrowserRouter>
   );
+
+  
 }
 
 export default App;
